002aabe4c4e09e9609623721a7a8fb2a38f6969c
marciopires <mpbox80@gmail.com>
2019-04-03 09:28:22 -0300

    repositorio com pasta blank

---
+++ blank-project-Copia/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Projeto em branco</title>
+	<link rel="stylesheet" href="">
+
+	<link rel="stylesheet/less" type="text/css" href="css/estilo.less" />
+	<script src="js/less.js" type="text/javascript"></script>
+	<script src="js/jquery.js" type="text/javascript"></script>
+
+
+<script>
+	jQuery(document).ready(function($) {
+		alert('jquery funcionando');
+	});
+</script>
+
+
+
+
+</head>
+<body>
+	<h1>Projeto em branco Marcio</h1>
+</body>
+</html>
